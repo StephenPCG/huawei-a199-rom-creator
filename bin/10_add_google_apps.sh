74c12325ba3f0292e52c6743e69a1cb270665adc
Zhang Cheng <StephenPCG@gmail.com>
2013-09-19 23:02:42 +0800

    adjust stockrom apps and google apps selection

--- bin/10_add_google_apps.sh
+++ bin/10_add_google_apps.sh
@@ -16,8 +16,15 @@ if [ ! -e "$gapps_zip" ]; then
     exit 1
 fi
 
+excludes=( "system/app/GenieWidget.apk" )   # weather and news
+excludes=( ${excludes[@]} system/app/GoogleFeedback.apk )
+excludes=( ${excludes[@]} 'system/tts/*' )
+excludes=( ${excludes[@]} 'optional/*' )
+excludes=( ${excludes[@]} 'META-INF/*' )
+excludes=( ${excludes[@]} 'install-optional.sh' )
+
 pushd $__work_dir >/dev/null 2>&1
-sudo unzip ../$gapps_zip -x 'optional/*' 'META-INF/*' install-optional.sh
+sudo unzip ../$gapps_zip -x ${excludes[@]}
 popd >/dev/null 2>&1
 
 # vim:ai:et:sts=4:sw=4:
